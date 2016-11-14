472af39cd27880c8e1bbe22dc2ab54c5fcca13b6
Simon Žlender <simon.zlender@gmail.com>
2016-11-14 19:30:11 +0100

    Clean up docker build script

--- script/docker_build.sh
+++ script/docker_build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run --rm -it -v $(pwd):/app elixir:1.3 bash -c 'export MIX_ENV=prod && cd /app && mix compile && mix release --env=prod'
+docker run --rm -it -v "$(pwd)":/app -w /app elixir:1.3 bash -c "
+  mix local.hex --force &&
+  MIX_ENV=prod mix compile &&
+  MIX_ENV=prod mix release --env=prod
+"
