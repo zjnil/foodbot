6c94466ae652eacf4d5071c3afba6f79fa143034
Simon Žlender <simon.zlender@gmail.com>
2016-11-14 18:05:12 +0100

    Fetch project version in deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,8 +5,11 @@ REL_NAME="$1"
 SSH_SERVER="$2"
 DEPLOY_PATH="$3"
 
+# get project version
+VERSION=$(mix run -e "IO.puts(Mix.Project.config[:version])")
+
 # upload release to server
-scp "rel/$REL_NAME/releases/*/$REL_NAME.tar.gz" "$SSH_SERVER:/$DEPLOY_PATH"
+scp rel/"$REL_NAME"/releases/"$VERSION"/"$REL_NAME.tar.gz" "$SSH_SERVER:/$DEPLOY_PATH"
 
 # extract and stop server (let the supervisor restart it)
 ssh "$SSH_SERVER" <<ENDSSH
