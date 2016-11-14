3f19801b347cd3148c43450228e7b28e60cb1dc2
Simon Žlender <simon.zlender@gmail.com>
2016-11-14 17:20:23 +0100

    Add $REL_NAME, $DEPLOY_PATH

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-SSH_SERVER="$1"
-SSH_PATH="$2"
+REL_NAME="$1"
+SSH_SERVER="$2"
+DEPLOY_PATH="$3"
 
 # upload release to server
-scp "rel/$REL_NAME/releases/*/$REL_NAME.tar.gz" "$SSH_SERVER:/$SSH_PATH"
+scp "rel/$REL_NAME/releases/*/$REL_NAME.tar.gz" "$SSH_SERVER:/$DEPLOY_PATH"
 
 # extract and stop server (let the supervisor restart it)
 ssh "$SSH_SERVER" <<ENDSSH
-  cd "$SSH_PATH"
+  cd "$DEPLOY_PATH"
   tar xvf "$REL_NAME.tar.gz"
   "bin/$REL_NAME" stop
 ENDSSH
