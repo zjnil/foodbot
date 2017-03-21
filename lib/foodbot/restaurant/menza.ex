22e12d75d35a2b184d02f8efebf56fb97b00d963
Simon Žlender <simon.zlender@gmail.com>
2017-03-21 10:52:05 +0100

    Fix missing menu items in menza

--- lib/foodbot/restaurant/menza.ex
+++ lib/foodbot/restaurant/menza.ex
@@ -20,7 +20,6 @@ defmodule Foodbot.Restaurant.Menza do
     title =
       list
       |> Enum.flat_map(fn p -> String.split(Floki.text(p), "\n") end)
-      |> Enum.drop(1)
       |> Enum.reject(&is_blank?/1)
       |> Enum.reject(&is_generic?/1)
       |> Enum.join(", ")
@@ -30,7 +29,7 @@ defmodule Foodbot.Restaurant.Menza do
   end
 
   def is_generic?(text) do
-    String.match?(text, ~r{solatni bife|dnevna sladica}i)
+    String.match?(text, ~r{solatni bife|dnevna sladica|juha}i)
   end
 
   def is_blank?(text) do
