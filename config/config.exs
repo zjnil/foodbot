29df1e99d85a67576da0ffe6a3f61e8bf6e65675
Simon Žlender <simon.zlender@gmail.com>
2018-04-11 10:25:29 +0200

    Disable Pauza

--- config/config.exs
+++ config/config.exs
@@ -9,7 +9,7 @@ config :foodbot, :port, {:system, "PORT", 4000}
 config :foodbot, restaurants: [
   gastro: Foodbot.Restaurant.Gastro,
   menza: Foodbot.Restaurant.Menza,
-  pauza: Foodbot.Restaurant.Pauza,
+  # pauza: Foodbot.Restaurant.Pauza,
   vinka: Foodbot.Restaurant.Vinka,
   kulinarnia: Foodbot.Restaurant.Kulinarnia
 ]
