8acff911f73b20f314af1e6e684b5bbb10fff0fd
zjerina <zjerina@nil.com>
2018-12-21 10:55:18 +0000

    Use Food group for roomId

--- webex_bot/config.py
+++ webex_bot/config.py
@@ -1,5 +1,5 @@
 ApiUrl = 'https://api.ciscospark.com/v1/messages'
-RoomId = 'Y2lzY29zcGFyazovL3VzL1JPT00vOGQzMGNhNjAtZmY3ZC0xMWU4LTg4YzMtMzMwNmUwYjE1YTUz' ## Example Room ID - https://developer.webex.com/
+RoomId = 'Y2lzY29zcGFyazovL3VzL1JPT00vYzBjMjcxNDAtMDUwMy0xMWU5LWEwYmEtYzdiMjQzZDhjNTll' ## Example Room ID - https://developer.webex.com/
 ContentType = 'application/json'
 BearerToken = 'Bearer MjA0NDFjMjMtNGM1MC00OGY4LWExNzMtNjU5OWEzYTdmMGE5NzE1NDFlYjItZTZi_PF84_73c15a99-fa49-4865-aa1a-4c730553576a' ## Example Auth token - https://developer.webex.com/
 SparkHeaders = {
